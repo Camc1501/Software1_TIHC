866b0167a0bebe9818b1377acd06eef66e3853e5
0rphen <the.bebop.88@gmail.com>
2017-10-17 15:02:41 -0500

    Git Update

--- Front/Software1TIHC/src/app/app.module.ts
+++ Front/Software1TIHC/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { ShipsComponent } from './components/ships/ships.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ShipsComponent
   ],
   imports: [
     BrowserModule
