63cae43b09db5ee81aaeaa5fcaa2478d95679676
0rphen <the.bebop.88@gmail.com>
2017-11-22 02:06:42 -0500

    Se agregan servicios

--- Front/Software1TIHC/src/app/service/employee.service.ts
+++ Front/Software1TIHC/src/app/service/employee.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Ship } from '../interface/ship';
+import { Employee } from '../interface/employee';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class ShipsService {
-    ship: Ship[] = [];
-    constructor(public http: HttpClient) {
-
-    }
-
-    getShip() {
+export class EmployeeService {
+    employee: Employee;
+    constructor(public http: HttpClient) { }
+    getEmployee() {
         let url: string = 'http://181.143.139.214:9763/services/TIHC_DSS.HTTPEndpoint/ships/1';
         let headers = new HttpHeaders({
             'Accept': 'application/json'
         });
         return this.http.get(url, { headers }).map((response: any) => {
-            this.ship.push(response.users.user);
-            return this.ship;
+            this.employee = response.users.user;
+            return this.employee;
         });
     }
 }
